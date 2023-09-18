e115d83f6b5b2ad882888acac07e170dc518658d
Nick Boldt <nboldt@redhat.com>
2023-09-18 11:12:35 -0300

    chore: fix multiarch support to be... (#99)
    * chore: fix multiarch support to be consistent with the way che-dashbord does it
    
    Change-Id: I99b2b296a5242855ffe7d39df1c622c846e36330
    Signed-off-by: Nick Boldt <nboldt@redhat.com>
    
    * remove unused method
    
    Change-Id: I563eef297041c6727da2c3ba59c3fbd11070cb02
    Signed-off-by: Nick Boldt <nboldt@redhat.com>
    
    ---------
    
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- make-release.sh
+++ make-release.sh
@@ -15,44 +15,63 @@
 # Used to create branch/tag, update VERSION files
 # and and trigger release by force pushing changes to the release branch
 
-# set to 1 to actually trigger changes in the release branch
-TRIGGER_RELEASE=0
+# set to 1 to actually tag the changes to the release branch
+TAG_RELEASE=0
 NOCOMMIT=0
-
-REGISTRY="quay.io"
-DOCKERFILE="build/dockerfiles/Dockerfile"
-ORGANIZATION="che-incubator"
-IMAGE="configbump"
 
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-    '-t'|'--trigger-release') TRIGGER_RELEASE=1; shift 0;;
+    '-t'|'--tag-release') TAG_RELEASE=1; NOCOMMIT=0; shift 0;;
     '-v'|'--version') VERSION="$2"; shift 1;;
-    '-n'|'--no-commit') NOCOMMIT=1; shift 0;;
+    '-n'|'--no-commit') NOCOMMIT=1; TAG_RELEASE=0; shift 0;;
   esac
   shift 1
 done
 
+bump_version () {
+  CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+  NEXT_VERSION=$1
+  BUMP_BRANCH=$2
+
+  git checkout "${BUMP_BRANCH}"
+
+  echo "Updating project version to ${NEXT_VERSION}"
+  update_pkgs_versions $NEXT_VERSION
+
+  if [[ ${NOCOMMIT} -eq 0 ]]; then
+    COMMIT_MSG="chore: Bump to ${NEXT_VERSION} in ${BUMP_BRANCH}"
+    git commit -asm "${COMMIT_MSG}"
+    git pull origin "${BUMP_BRANCH}"
+
+    set +e
+    PUSH_TRY="$(git push origin "${BUMP_BRANCH}")"
+    # shellcheck disable=SC2181
+    if [[ $? -gt 0 ]] || [[ $PUSH_TRY == *"protected branch hook declined"* ]]; then
+        # create pull request for main branch, as branch is restricted
+        PR_BRANCH=pr-${BUMP_BRANCH}-to-${NEXT_VERSION}
+        git branch "${PR_BRANCH}"
+        git checkout "${PR_BRANCH}"
+        git pull origin "${PR_BRANCH}"
+        git push origin "${PR_BRANCH}"
+        lastCommitComment="$(git log -1 --pretty=%B)"
+        hub pull-request -f -m "${lastCommitComment}" -b "${BUMP_BRANCH}" -h "${PR_BRANCH}"
+    fi
+    set -e
+  fi
+  git checkout "${CURRENT_BRANCH}"
+}
+
 usage ()
 {
-  echo "Usage: $0 --version [VERSION TO RELEASE] [--trigger-release]"
-  echo "Example: $0 --version 7.75.0 --trigger-release"; echo
+  echo "Usage: $0 --version [VERSION TO RELEASE] [--tag-release]"
+  echo "Example: $0 --version 7.75.0 --tag-release"; echo
 }
 
 if [[ ! ${VERSION} ]]; then
   usage
   exit 1
 fi
-
-releaseMachineExec() {
-  # docker buildx includes automated push to registry, so build using tag we want published, not just local ${IMAGE}
-  docker buildx build \
-    --tag "${REGISTRY}/${ORGANIZATION}/${IMAGE}:${VERSION}" \
-    --push \
-    --provenance=false \
-    -f ./${DOCKERFILE} . --platform "linux/amd64,linux/arm64" | cat
-  echo "Pushed ${REGISTRY}/${ORGANIZATION}/${IMAGE}:${VERSION}"
-}
 
 # derive branch from version
 BRANCH=${VERSION%.*}.x
@@ -64,16 +83,18 @@ else
   BASEBRANCH="${BRANCH}"
 fi
 
+# get sources from ${BASEBRANCH} branch
+git fetch origin "${BASEBRANCH}":"${BASEBRANCH}"
+git checkout "${BASEBRANCH}"
+
 # create new branch off ${BASEBRANCH} (or check out latest commits if branch already exists), then push to origin
 if [[ "${BASEBRANCH}" != "${BRANCH}" ]]; then
   git branch "${BRANCH}" || git checkout "${BRANCH}" && git pull origin "${BRANCH}"
   git push origin "${BRANCH}"
-  git fetch origin "${BRANCH}:${BRANCH}" || true
+  git fetch origin "${BRANCH}:${BRANCH}"
   git checkout "${BRANCH}"
-else
-  git fetch origin "${BRANCH}:${BRANCH}" || true
-  git checkout ${BRANCH}
 fi
+
 set -e
 
 # change VERSION file
@@ -82,57 +103,29 @@ echo "${VERSION}" > VERSION
 # commit change into branch
 if [[ ${NOCOMMIT} -eq 0 ]]; then
   COMMIT_MSG="chore: release: bump to ${VERSION} in ${BRANCH}"
-  git commit -s -m "${COMMIT_MSG}" VERSION
+  git commit -asm "${COMMIT_MSG}" VERSION
   git pull origin "${BRANCH}"
   git push origin "${BRANCH}"
 fi
 
-if [[ $TRIGGER_RELEASE -eq 1 ]]; then
-  # push new branch to release branch to trigger CI build
-  releaseConfigbump
-
+if [[ $TAG_RELEASE -eq 1 ]]; then
   # tag the release
   git checkout "${BRANCH}"
   git tag "${VERSION}"
   git push origin "${VERSION}"
 fi
 
-# now update ${BASEBRANCH} to the new snapshot version
-git fetch origin "${BASEBRANCH}":"${BASEBRANCH}" || true
+# now update ${BASEBRANCH} to the new version
 git checkout "${BASEBRANCH}"
 
 # change VERSION file + commit change into ${BASEBRANCH} branch
 if [[ "${BASEBRANCH}" != "${BRANCH}" ]]; then
-  # bump the y digit
+  # bump the y digit, if it is a major release
   [[ $BRANCH =~ ^([0-9]+)\.([0-9]+)\.x ]] && BASE=${BASH_REMATCH[1]}; NEXT=${BASH_REMATCH[2]}; (( NEXT=NEXT+1 )) # for BRANCH=7.10.x, get BASE=7, NEXT=11
-  NEXTVERSION="${BASE}.${NEXT}.0-SNAPSHOT"
-else
-  # bump the z digit
-  [[ $VERSION =~ ^([0-9]+)\.([0-9]+)\.([0-9]+) ]] && BASE="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}"; NEXT="${BASH_REMATCH[3]}"; (( NEXT=NEXT+1 )) # for VERSION=7.7.1, get BASE=7.7, NEXT=2
-  NEXTVERSION="${BASE}.${NEXT}-SNAPSHOT"
+  NEXT_VERSION_Y="${BASE}.${NEXT}.0-next"
+  bump_version "${NEXT_VERSION_Y}" "${BASEBRANCH}"
 fi
-
-# change VERSION file
-echo "${NEXTVERSION}" > VERSION
-if [[ ${NOCOMMIT} -eq 0 ]]; then
-  BRANCH=${BASEBRANCH}
-  # commit change into branch
-  COMMIT_MSG="chore: release: bump to ${NEXTVERSION} in ${BRANCH}"
-  git commit -s -m "${COMMIT_MSG}" VERSION
-  git pull origin "${BRANCH}"
-
-  PUSH_TRY="$(git push origin "${BRANCH}")"
-  # shellcheck disable=SC2181
-  if [[ $? -gt 0 ]] || [[ $PUSH_TRY == *"protected branch hook declined"* ]]; then
-  PR_BRANCH=pr-main-to-${NEXTVERSION}
-    # create pull request for main branch, as branch is restricted
-    git branch "${PR_BRANCH}"
-    git checkout "${PR_BRANCH}"
-    git pull origin "${PR_BRANCH}"
-    git push origin "${PR_BRANCH}"
-    lastCommitComment="$(git log -1 --pretty=%B)"
-    hub pull-request -f -m "${lastCommitComment}
-
-${lastCommitComment}" -b "${BRANCH}" -h "${PR_BRANCH}"
-  fi
-fi
+# bump the z digit
+[[ $VERSION =~ ^([0-9]+)\.([0-9]+)\.([0-9]+) ]] && BASE="${BASH_REMATCH[1]}.${BASH_REMATCH[2]}"; NEXT="${BASH_REMATCH[3]}"; (( NEXT=NEXT+1 )) # for VERSION=7.7.1, get BASE=7.7, NEXT=2
+NEXT_VERSION_Z="${BASE}.${NEXT}-next"
+bump_version "${NEXT_VERSION_Z}" "${BRANCH}"
